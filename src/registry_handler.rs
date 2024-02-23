eb2840e8e9351ec5b4738581f7936a7593e49c42
behrooz3500 <behrooz3500@yahoo.com>
2024-02-23 14:11:08 +0330

    add a method for displaying ok message box

--- src/registry_handler.rs
+++ src/registry_handler.rs
@@ -130,3 +130,24 @@ pub fn schedule_setup_task() -> std::io::Result<()> {
     }
     Ok(())
 }
+
+
+pub fn message_box(window_title: &str, window_message: &str) {
+    let wide_error_message: Vec<u16> = OsStr::new(window_message)
+        .encode_wide()
+        .chain(Some(0).into_iter())
+        .collect();
+    let wide_window_title: Vec<u16> = OsStr::new(window_title)
+        .encode_wide()
+        .chain(Some(0).into_iter())
+        .collect();
+    unsafe {
+        MessageBoxW(
+            ptr::null_mut(),
+            wide_error_message.as_ptr(),
+            wide_window_title.as_ptr(),
+            MB_OK | MB_ICONERROR,
+        );
+    };
+}
+

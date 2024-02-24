cdc96e7eaa6a710ddcaed3414e07ec0559eacdb5
behrooz3500 <behrooz3500@yahoo.com>
2024-02-24 11:30:38 +0330

    handle exceptions gracefully

--- src/registry_handler.rs
+++ src/registry_handler.rs
@@ -14,7 +14,7 @@ use winreg::enums::{HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, KEY_ALL_ACCESS};
 
 
 
-pub fn is_rebooted() -> bool {
+pub fn _is_rebooted() -> bool {
     let hklm: RegKey = RegKey::predef(HKEY_LOCAL_MACHINE);
     match hklm.open_subkey_with_flags(REBOOT_REGISTRY_PATH, KEY_ALL_ACCESS) {
         Ok(reg_key) => match reg_key.get_value::<String, &str>("rebooted") {
@@ -44,29 +44,36 @@ pub fn set_rebooted_key(value: i8) -> std::io::Result<()> {
 }
 
 pub fn export_registry_key() -> std::io::Result<()> {
+
     let current_path: PathBuf = env::current_dir().expect("failed to get the current directory");
     let local_path_header: &str = "HKEY_LOCAL_MACHINE";
     let user_path_header: &str = "HKEY_CURRENT_USER";
-    let registry_headers: [&str; 2] = [local_path_header, user_path_header];
+    let hklm: RegKey = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let hkcu: RegKey = RegKey::predef(HKEY_CURRENT_USER);
 
-    for header in &registry_headers {
+    let registry_headers: [(&str, RegKey); 2] = [(local_path_header, hklm), (user_path_header, hkcu)];
+
+    for (header, reg_key) in &registry_headers {
         let output_file: String = format!("{}.reg", header.to_lowercase());
         let full_path = current_path.join(DATA_FOLDER_NAME).join(output_file);
 
-        match fs::File::create(&full_path) {
-            Ok(_) => {}
-            Err(err) => return Err(err),
-        }
+        match reg_key.open_subkey_with_flags(REGISTRY_STARTUP_PATH, KEY_ALL_ACCESS) {
+            Ok(_) => {
+                
+                match fs::File::create(&full_path) {
+                    Ok(_) => {}
+                    Err(err) => return Err(err),
+                }
 
-        let status = Command::new("regedit")
-            .arg("/e")
-            .arg(&full_path)
-            .arg(format!("{}\\{}", header, REGISTRY_STARTUP_PATH))
-            .status()
-            .expect("failed");
+                let _status = Command::new("regedit")
+                    .arg("/e")
+                    .arg(&full_path)
+                    .arg(format!("{}\\{}", header, REGISTRY_STARTUP_PATH))
+                    .status()?;
 
-        if !status.success() {
-            return Err(io::Error::new(io::ErrorKind::Other, "failed"));
+                
+            },
+            Err(_) => return Err(io::Error::new(io::ErrorKind::Other, format!("failed to open {} registry keys, probably they do not exist", header)))
         }
     }
     Ok(())
@@ -83,20 +90,16 @@ pub fn delete_registry_key() -> std::io::Result<()> {
     for (header, reg_key) in &registry_headers {
         match reg_key.open_subkey_with_flags(REGISTRY_STARTUP_PATH, KEY_ALL_ACCESS) {
             Ok(_) => {
-                let status = Command::new("cmd")
+                let _status = Command::new("cmd")
                     .arg("/C")
                     .arg("reg")
                     .arg("delete")
                     .arg(format!("{}\\{}", header, REGISTRY_STARTUP_PATH))
                     .arg("/f")
-                    .status()
-                    .expect("failed to delete registry key");
+                    .status()?;
 
-                if !status.success() {
-                    return Err(io::Error::new(io::ErrorKind::Other, "failed"));
-                }
             }
-            Err(_) => println!("error deleting registry key {}", header),
+            Err(_) => return Err(io::Error::new(io::ErrorKind::Other, format!("failed to open {} registry keys, probably they do not exist", header))),
         }
     }
     Ok(())
@@ -106,7 +109,7 @@ pub fn schedule_setup_task() -> std::io::Result<()> {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let current_path = env::current_dir()?;
     let setup_path = current_path.join(DATA_FOLDER_NAME).join(SETUP_EXE_NAME);
-    println!("setup path: {}", setup_path.to_str().unwrap());
+    let registry_restore_path = current_path.join(DATA_FOLDER_NAME).join(REGISTRY_RESTORE_EXECUTABLE);
 
     match hklm.open_subkey_with_flags(REGISTRY_RUNONCE_PATH, KEY_ALL_ACCESS) {
         Ok(reg_key) => {
@@ -115,7 +118,16 @@ pub fn schedule_setup_task() -> std::io::Result<()> {
                 Err(_) => {
                     return Err(io::Error::new(
                         io::ErrorKind::Other,
-                        "failed setting value for task schedule",
+                        "failed setting value for setup task schedule probably due to permissions",
+                    ))
+                }
+            }
+            match reg_key.set_value("registry_restore", &registry_restore_path.to_str().unwrap()) {
+                Ok(_) => {}
+                Err(_) => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        "failed setting value for registry restore task schedule probably due to permissions",
                     ))
                 }
             }

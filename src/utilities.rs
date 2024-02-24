b0e40f6a27d9a38d6f39516d0ce6fd50effd0738
behrooz3500 <behrooz3500@yahoo.com>
2024-02-24 11:29:56 +0330

    add logger function

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,13 +1,25 @@
 // std
 use std::ptr;
 use std::ffi::OsStr;
+use std::error::Error;
 use std::os::windows::ffi::OsStrExt;
 
 // winapi
-use winapi::um::winuser::{MessageBoxW, MB_ICONINFORMATION, MB_OK};
+use winapi::um::winuser::{MessageBoxW, MB_ICONINFORMATION, MB_ICONWARNING, MB_ICONERROR, MB_OK};
+
+// log
+use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+
+pub enum WindowType {
+    _Error,
+    Information,
+    _Warning,
+}
 
 
-pub fn message_box(window_title: &str, window_message: &str) {
+pub fn message_box(window_title: &str, window_message: &str, window_type: WindowType) {
     let wide_error_message: Vec<u16> = OsStr::new(window_message)
         .encode_wide()
         .chain(Some(0))
@@ -21,8 +33,27 @@ pub fn message_box(window_title: &str, window_message: &str) {
             ptr::null_mut(),
             wide_error_message.as_ptr(),
             wide_window_title.as_ptr(),
-            MB_OK | MB_ICONINFORMATION,
+            MB_OK | match window_type {
+                WindowType::_Error => MB_ICONERROR,
+                WindowType::Information => MB_ICONINFORMATION,
+                WindowType::_Warning => MB_ICONWARNING,
+            },
         );
     };
+}
+
+
+pub fn setup_logging() -> Result<(), Box<dyn Error>> {
+    let log_file = FileAppender::builder()
+        .append(true)
+        .build("data/logs/setup_assistant.log")?;
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(log_file)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
+
+    log4rs::init_config(config)?;
+    Ok(())
 }
 

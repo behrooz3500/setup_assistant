e7f8c60967404b7e0c68abaa827aec04b3fc8862
behrooz3500 <behrooz3500@yahoo.com>
2024-02-27 22:02:09 +0330

    restore registry files based on their registry view path

--- external_resources/registy_restore/src/main.rs
+++ external_resources/registy_restore/src/main.rs
@@ -1,22 +1,77 @@
+use std::fs;
 use std::env;
+use std::error::Error;
 use std::path::PathBuf;
 use std::process::Command;
 
-mod constants;
-use constants::{LOCAL_REGISTRY_KEY, USER_REGISTRY_KEY};
+use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+
+// mod constants;
+// use constants::{LOCAL_REGISTRY_KEY, USER_REGISTRY_KEY};
 
 fn main() {
     let current_dir: PathBuf = env::current_dir().expect("Failed to get current directory");
-    let local_registry_path = current_dir.join(LOCAL_REGISTRY_KEY);
-    let user_registry_path = current_dir.join(USER_REGISTRY_KEY);
-    let registry_paths: [&str; 2] = [local_registry_path.to_str().unwrap(), user_registry_path.to_str().unwrap()];
+    setup_logging().expect("Failed to setup logging");
 
-    for path in &registry_paths {
-        let mut import_command = Command::new("reg");
-        import_command
-            .args(["import", path])
-            .output()
-            .expect("Failed to execute command");
+    match fs::read_dir(current_dir) {
+        Ok(entries) => {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let file_name = entry.path();
+                    if  file_name.extension().is_some() && file_name.extension().unwrap() == "reg" {
+                        let is_64bit_view = file_name.to_str().unwrap().contains("64");
+                        println!("Found registry file: {}", file_name.display());
+                        match restore_registry(entry.path(), is_64bit_view) {
+                            Ok(_) => (),
+                            Err(err) => log::info!("Error restoring registry: {}", err),
+                        }
+                    }
+                }
+            }
+        }
+        Err(err) => println!("Error reading directory: {}", err),
     }
 }
 
+fn restore_registry(file_path: PathBuf, is_64bit_view: bool) -> std::io::Result<()> {
+    let mut import_command = Command::new("reg");
+    import_command.arg("import");
+    import_command.arg(&file_path);
+    if is_64bit_view {   
+        import_command.arg("/reg:64");
+    }
+    match import_command.status() {
+        Ok(status) => {
+            if status.success() {
+                log::info!("registry at {} imported successfully", file_path.display());
+                Ok(())
+            } else {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    format!("Failed to import registry file {}", file_path.display()),
+                ))
+            }
+        }
+        Err(err) => {
+            log::error!("Importing {} completely failed.", file_path.display());
+            Err(err)
+        }
+    }
+}
+
+
+fn setup_logging() -> Result<(), Box<dyn Error>> {
+    let log_file = FileAppender::builder()
+        .append(true)
+        .build("logs/setup_assistant.log")?;
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(log_file)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
+
+    log4rs::init_config(config)?;
+    Ok(())
+}

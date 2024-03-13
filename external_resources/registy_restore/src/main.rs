ee09bdbfdf372a48af5537f0413b61e382a725e1
behrooz3500 <behrooz3500@yahoo.com>
2024-03-13 12:49:45 +0330

    remove unused imports

--- external_resources/registy_restore/src/main.rs
+++ external_resources/registy_restore/src/main.rs
@@ -5,7 +5,6 @@ use std::path::PathBuf;
 use std::process::Command;
 
 use log::LevelFilter;
-use log4rs::append::file;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 
